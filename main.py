68588ae68a779a4ff57bf8b0c3d4ece6bce6a4b5
manuel-labbe <m.labbe01@ufromail.cl>
2024-03-26 13:07:49 -0300

    nuevas funciones barra de carga, deposito y add

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$misiones ', intents=intents)
-cols = ['Numero', 'Mision', 'Valor']
+cols = ['Numero', 'Mision', 'Valor', 'Saldo']
 df = pd.DataFrame(columns = cols)
 misiones = tabla_misiones(tabla_misiones = df)
 
@@ -28,7 +28,10 @@ async def add(ctx, *, arg):
     tabla_misiones = misiones.nueva_mision(nombre=nombre,valor=valor)
     texto_df = ''
     for idx, cols in enumerate(tabla_misiones.Mision):
-        texto_df += f'{idx+1}. {cols} ${tabla_misiones.Valor.iloc[idx]}\n'
+        total = tabla_misiones.Valor.iloc[idx]
+        saldo = tabla_misiones.Saldo.iloc[idx]
+        texto_df += f'{idx+1}. {cols} ${tabla_misiones.Valor.iloc[idx]}, Saldo: ${saldo} {barra_de_carga(saldo)}\n'
+    #texto_df += f'test: {tabla_misiones}'
     embed = discord.Embed(title='Misiones',
                           description= texto_df,
                           color=0x00ff00)
@@ -36,13 +39,15 @@ async def add(ctx, *, arg):
 
 @bot.command()
 async def deposito(ctx, numero_mision: int, valor_deposito: int):
-    pos_mision = tabla_misiones.loc[tabla_misiones['Numero'] == numero_mision]
+    pos = tabla_misiones['Numero'] == numero_mision
+    pos_mision = tabla_misiones.loc[pos]
     saldo = balance(int(pos_mision.Valor.iloc[0]))
     deposito = saldo.deposito(valor_deposito=int(valor_deposito))
-    text = f'Se deposita {valor_deposito} para mision {numero_mision} con valor de {pos_mision.Valor.iloc[0]} saldo actual de la mision {saldo.saldo_actual} con porcentaje de {saldo.porcentaje}'
+    text = f'Se deposita {valor_deposito} para mision {numero_mision} con valor de {pos_mision.Valor.iloc[0]} saldo actual de la mision {saldo.saldo_actual} lleva un {saldo.porcentaje:.2f}%'
     embed = discord.Embed(title='Deposito',
                           description= text,
                           color=0x00ff00)
+    tabla_misiones['Saldo'].loc[pos] = saldo.saldo_actual
     await ctx.send(embed=embed)
 
 
@@ -54,4 +59,4 @@ $mision deposito [nr.mision] [valor deposito] -> suma saldo a la misión
 $mision stats -> barra de progreso
 """
 
-bot.run('token')
+bot.run('token here')
--- util.py
+++ util.py
@@ -3,11 +3,12 @@ import numpy as np
 
 
 class tabla_misiones:
-    def __init__(self, tabla_misiones):
+    def __init__(self, tabla_misiones, saldo=0):
         self.misiones = tabla_misiones
+        self.saldo = saldo
     def nueva_mision(self, nombre,valor):
         numero = self.misiones.shape[0] + 1
-        new = pd.DataFrame({'Numero': [numero], 'Mision': [nombre], 'Valor': [valor]})
+        new = pd.DataFrame({'Numero': [numero], 'Mision': [nombre], 'Valor': [valor], 'Saldo': [self.saldo]})
         self.misiones = pd.concat([self.misiones,new], ignore_index=True)
         self.misiones = self.misiones.reset_index(drop=True)
         return self.misiones
@@ -17,9 +18,9 @@ def barra_de_carga(porcentaje):
     valor = porcentaje/100
     barra = '[:-------------------------]'
     rango = int(21*valor)
-    barra = f"[{'X' * rango}{'-' * (20 - rango)}]"
+    barra = f"[{'X' * rango}{'-' * (20 - rango)}] {porcentaje}%"
     if(valor == 1):
-        return 'Fondos para la misión completados!'
+        return '¡Fondos para la misión completados!'
     return barra
 
     
